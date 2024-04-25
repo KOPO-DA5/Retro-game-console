4c9e0d947057b576b377609cac75da2c6c480cf8
wldnjs7064 <wldnjs7064@naver.com>
2024-04-25 10:04:02 +0900

    ✨ feat: css 수정

--- js/tetris.js
+++ js/tetris.js
@@ -27,7 +27,9 @@ function updateGameContent() {
                     <h2>HIGH SCORES</h2>
                     <ol id="highScores"></ol>
                   </div>
-                 <canvas id="board" class="game-board"></canvas>
+                  <div class="game-board-container">
+                  <canvas id="board" class="game-board"></canvas>
+                  </div>
                  <div class="right-column">
                     <div>
                         <h1>TETRIS</h1>
