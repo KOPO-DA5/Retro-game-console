3db058f892c1733054b152ae5444cfb11519ab1a
wldnjs7064 <wldnjs7064@naver.com>
2024-04-26 00:42:30 +0900

    ✨ feat: 테트리스 js 여러 번 로드 시 삭제

--- js/tetris.js
+++ js/tetris.js
@@ -17,7 +17,14 @@ function resetAnimation(element) {
   void element.offsetWidth; // DOM 리플로우 강제 실행으로 CSS 애니메이션 리셋
   element.classList.add("fade-in");
 }
+// 각 스크립트 파일의 로드 상태를 추적하기 위한 변수들
+let constantsLoaded = false;
+let boardLoaded = false;
+let pieceLoaded = false;
+let mainLoaded = false;
+let soundLoaded = false;
 
+// updateGameContent 함수 내에서 호출
 function updateGameContent() {
   const content = document.getElementById("content");
   content.innerHTML = `
@@ -44,32 +51,77 @@ function updateGameContent() {
                     </div>
                     <button onclick="play()" class="play-button">Play</button>
                 </div>
-                
             `;
-  /* js부르는 법 */
 
-  let scriptToConstants = document.createElement("script");
-  scriptToConstants.type = "text/javascript";
-  scriptToConstants.src = "./js/tetris/constants.js";
-  document.body.appendChild(scriptToConstants);
+  // constants.js가 로드되지 않았다면 로드
+  if (!constantsLoaded) {
+    let scriptToConstants = document.createElement("script");
+    scriptToConstants.type = "text/javascript";
+    scriptToConstants.src = "./js/tetris/constants.js";
+    document.body.appendChild(scriptToConstants);
+    constantsLoaded = true;
+  }
 
-  let scriptToBoard = document.createElement("script");
-  scriptToBoard.type = "text/javascript";
-  scriptToBoard.src = "./js/tetris/board.js";
-  document.body.appendChild(scriptToBoard);
+  // board.js가 로드되지 않았다면 로드
+  if (!boardLoaded) {
+    let scriptToBoard = document.createElement("script");
+    scriptToBoard.type = "text/javascript";
+    scriptToBoard.src = "./js/tetris/board.js";
+    document.body.appendChild(scriptToBoard);
+    boardLoaded = true;
+  }
 
-  let scriptToPiece = document.createElement("script");
-  scriptToPiece.type = "text/javascript";
-  scriptToPiece.src = "./js/tetris/piece.js";
-  document.body.appendChild(scriptToPiece);
+  // piece.js가 로드되지 않았다면 로드
+  if (!pieceLoaded) {
+    let scriptToPiece = document.createElement("script");
+    scriptToPiece.type = "text/javascript";
+    scriptToPiece.src = "./js/tetris/piece.js";
+    document.body.appendChild(scriptToPiece);
+    pieceLoaded = true;
+  }
 
-  let scriptTomain = document.createElement("script");
-  scriptTomain.type = "text/javascript";
-  scriptTomain.src = "./js/tetris/main.js";
-  document.body.appendChild(scriptTomain);
+  // main.js가 로드되지 않았다면 로드
+  if (!mainLoaded) {
+    let scriptTomain = document.createElement("script");
+    scriptTomain.type = "text/javascript";
+    scriptTomain.src = "./js/tetris/main.js";
+    document.body.appendChild(scriptTomain);
+    mainLoaded = true;
+  }
 
-  let scriptTosound = document.createElement("script");
-  scriptTosound.type = "text/javascript";
-  scriptTosound.src = "./js/tetris/sound.js";
-  document.body.appendChild(scriptTosound);
+  // sound.js가 로드되지 않았다면 로드
+  if (!soundLoaded) {
+    let scriptTosound = document.createElement("script");
+    scriptTosound.type = "text/javascript";
+    scriptTosound.src = "./js/tetris/sound.js";
+    document.body.appendChild(scriptTosound);
+    soundLoaded = true;
+  }
+
+  // 이미 로드된 스크립트를 삭제
+  if (constantsLoaded) {
+    document
+      .querySelectorAll('script[src="./js/tetris/constants.js"]')
+      .forEach((script) => script.remove());
+  }
+  if (boardLoaded) {
+    document
+      .querySelectorAll('script[src="./js/tetris/board.js"]')
+      .forEach((script) => script.remove());
+  }
+  if (pieceLoaded) {
+    document
+      .querySelectorAll('script[src="./js/tetris/piece.js"]')
+      .forEach((script) => script.remove());
+  }
+  if (mainLoaded) {
+    document
+      .querySelectorAll('script[src="./js/tetris/main.js"]')
+      .forEach((script) => script.remove());
+  }
+  if (soundLoaded) {
+    document
+      .querySelectorAll('script[src="./js/tetris/sound.js"]')
+      .forEach((script) => script.remove());
+  }
 }
